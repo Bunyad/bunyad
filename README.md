a45e9d683b8ee725b97951b679ea7c1e0b0b818e
Adnan Khan <lodhi79@gmail.com>
2013-10-10 09:21:46 +1100

    Landing page and rationale

---
+++ README.md
@@ -0,0 +1,4 @@
+landing_page
+============
+
+Landing page to get people interested in the project and share with others. This can be a preamble to our teacher registration page.
---
+++ anchor/config/application.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+	// Application URL
+	'url' => '/',
+
+	// Application Index
+	'index' => 'index.php',
+
+	// Application Timezone
+	'timezone' => 'Europe/London',
+
+	// Application Key
+	'key' => 'lXePiy8WmUa0t0nYnPy3UVc1hH8gCB2iAis96W3O',
+
+	// Default Application Language
+	'language' => 'en_GB',
+
+	// Application Character Encoding
+	'encoding' => 'UTF-8'
+);
